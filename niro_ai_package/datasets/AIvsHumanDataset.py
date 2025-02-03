771572d8a9c1b5691192b1173ff0d80623961db1
IEPiXI <krg.rbrt@gmail.com>
2025-02-03 17:30:35 +0100

    Updated the KaggleDataset Download to use the correct path
    * the kaggle dataset now uses the correct path for downloading its data
    * updated the config to have a 'datasets' path instead of just 'data'

--- niro_ai_package/datasets/AIvsHumanDataset.py
+++ niro_ai_package/datasets/AIvsHumanDataset.py
@@ -3,4 +3,4 @@ from niro_ai_package.datasets.KaggleDataset import KaggleDataset
 
 class AIvsHumanDataset(KaggleDataset):
     def __init__(self, dataset_config):
-        super().__init__(dataset_config, kaggle_path="alessandrasala79/ai-vs-human-generated-dataset")
+        super().__init__(dataset_config, kaggle_path='alessandrasala79/ai-vs-human-generated-dataset')
--- niro_ai_package/datasets/KaggleDataset.py
+++ niro_ai_package/datasets/KaggleDataset.py
@@ -1,12 +1,17 @@
-from niro_ai_package.datasets.BaseDataset import BaseDataset
+import os
+
 import kagglehub
+
+from niro_ai_package.datasets.BaseDataset import BaseDataset
+
 
 class KaggleDataset(BaseDataset):
     def __init__(self, dataset_config, kaggle_path):
         super().__init__(dataset_config)
+        # use the parent of the data_path as kaggle already downloads to '/datasets'
+        os.environ['KAGGLEHUB_CACHE'] = str(self.data_path.parent.resolve())
         self.kaggle_path = kaggle_path
 
     def download(self):
         path = kagglehub.dataset_download(self.kaggle_path)
         return path
-
